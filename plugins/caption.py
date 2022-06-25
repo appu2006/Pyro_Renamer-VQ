55bf5256281ab1377f5aacb5b4d3d9601fba10ce
appu2006 <93041202+appu2006@users.noreply.github.com>
2022-06-25 13:38:56 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters 
-from helper.database import find, addcaption, delcaption 
+from helper.database import find, addcaption, delcaption
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
@@ -21,6 +22,12 @@ async def delete_caption(client, message):
 async def see_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if caption:
-       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
+       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`"
+           reply_markup=InlineKeyboardMarkup( [[
+                InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/Powerupdate'),
+                InlineKeyboardButton('🍂 𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url='https://t.me/Powersupporthelp')
+              ]]                   
+             )
+       )                  
     else:
        await message.reply_text("**You dont have any custom caption**")
